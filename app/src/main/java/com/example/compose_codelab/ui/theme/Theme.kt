00dd3ad8634974ed92d243e57d204629e94cdbf8
honghyk <bhh970207@gmail.com>
2022-01-11 22:43:11 +0900

    codelab step 1~5

--- app/src/main/java/com/example/compose_codelab/ui/theme/Theme.kt
+++ app/src/main/java/com/example/compose_codelab/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ComposecodelabTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun ComposeCodelabTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
   val colors = if (darkTheme) {
     DarkColorPalette
   } else {
