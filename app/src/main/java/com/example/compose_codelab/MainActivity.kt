c6c08b7d93870b14ad30279a0015fb9465e8be68
honghyk <bhh970207@gmail.com>
2022-01-13 22:13:06 +0900

    codelab step 9 - Creating a performant lazy list
    - LazyColumn and LazyRow are equivalent to RecyclerView
    - LazyColumn doesn't recycler its children like RecyclerView. It emits new Composables.

--- app/src/main/java/com/example/compose_codelab/MainActivity.kt
+++ app/src/main/java/com/example/compose_codelab/MainActivity.kt
@@ -8,21 +8,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_codelab.ui.theme.ComposeCodelabTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,10 +78,10 @@ fun OnboardingPreview() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-  Column(modifier = Modifier.padding(vertical = 4.dp)) {
-    for (name in names) {
-      Greeting(name)
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+  LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+    items(items = names) { name ->
+      Greeting(name = name)
     }
   }
 }
