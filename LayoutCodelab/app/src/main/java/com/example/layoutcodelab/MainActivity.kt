2a9152553b594ddea90bf4b60d21dea355099ae2
honghyk <bhh970207@gmail.com>
2022-01-18 00:50:19 +0900

    step 6 - Working with lists
    - LazyColumn is the equivalent of RecyclerView
    - To avoid blocking the list rendering while we scroll, the scrolling APIs are suspend functions. Therefore, we need to call them in a coroutine.

--- LayoutCodelab/app/src/main/java/com/example/layoutcodelab/MainActivity.kt
+++ LayoutCodelab/app/src/main/java/com/example/layoutcodelab/MainActivity.kt
@@ -3,8 +3,16 @@ package com.example.layoutcodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -13,23 +21,22 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.layoutcodelab.ui.theme.LayoutCodelabTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.example.layoutcodelab.ui.theme.LayoutCodelabTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
       LayoutCodelabTheme {
-        LayoutsCodelab()
+        LazyList()
       }
     }
   }
@@ -66,10 +73,69 @@ fun BodyContent(modifier: Modifier = Modifier) {
   }
 }
 
+@Composable
+fun LazyList() {
+  val listSize = 100
+  val scrollState = rememberLazyListState()
+  val coroutineScope = rememberCoroutineScope()
+
+  Column {
+    Row {
+      Button(
+        modifier = Modifier.weight(1f),
+        onClick = {
+          coroutineScope.launch { scrollState.animateScrollToItem(0) }
+        }) {
+        Text("Scroll to the top")
+      }
+      Button(
+        modifier = Modifier.weight(1f),
+        onClick = {
+          coroutineScope.launch { scrollState.animateScrollToItem(listSize - 1) }
+        }) {
+        Text("Scroll to the end")
+      }
+    }
+
+    LazyColumn(state = scrollState) {
+      items(100) {
+        ImageListItem(it)
+      }
+    }
+  }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+  Row(verticalAlignment = Alignment.CenterVertically) {
+    Image(
+      painter = rememberImagePainter(
+        data = "https://developer.android.com/images/brand/Android_Robot.png"
+      ),
+      contentDescription = "Android Logo",
+      modifier = Modifier.size(50.dp)
+    )
+    Spacer(Modifier.width(10.dp))
+    Text("Item $index", style = MaterialTheme.typography.subtitle1)
+  }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun LayoutsCodelabPreview() {
   LayoutCodelabTheme {
     LayoutsCodelab()
+  }
+}
+
+@Preview(
+  showBackground = true,
+  widthDp = 320,
+  heightDp = 480
+)
+@Composable
+fun LazyListPreview() {
+  LayoutCodelabTheme {
+    LazyList()
   }
 }
