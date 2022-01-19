2ae8b9c4daa63e14bac0d98c0e852dd0ec7e5b91
honghyk <bhh970207@gmail.com>
2022-01-19 22:55:38 +0900

    step 8 - Complex custom layout

--- LayoutCodelab/app/src/main/java/com/example/layoutcodelab/MainActivity.kt
+++ LayoutCodelab/app/src/main/java/com/example/layoutcodelab/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.layoutcodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,7 +16,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -25,6 +32,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.Layout
@@ -35,13 +43,14 @@ import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.layoutcodelab.ui.theme.LayoutCodelabTheme
 import kotlinx.coroutines.launch
+import kotlin.math.max
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
       LayoutCodelabTheme {
-        LazyList()
+        LayoutsCodelab()
       }
     }
   }
@@ -70,12 +79,20 @@ fun LayoutsCodelab() {
   }
 }
 
+val topics = listOf(
+  "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+  "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+  "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-  MyOwnColumn(modifier = modifier.padding(8.dp)) {
-    Text("MyOwnColumn")
-    Text(text = "Hi there!")
-    Text(text = "Thanks for going through the Layouts codelab")
+  Row(modifier = Modifier.horizontalScroll(rememberScrollState())) {
+    StaggeredGrid(modifier = modifier, rows = 5) {
+      for (topic in topics) {
+        Chip(modifier = Modifier.padding(8.dp), text = topic)
+      }
+    }
   }
 }
 
@@ -111,6 +128,18 @@ fun LazyList() {
   }
 }
 
+@Preview(
+  showBackground = true,
+  widthDp = 320,
+  heightDp = 480
+)
+@Composable
+fun LazyListPreview() {
+  LayoutCodelabTheme {
+    LazyList()
+  }
+}
+
 @Composable
 fun ImageListItem(index: Int) {
   Row(verticalAlignment = Alignment.CenterVertically) {
@@ -143,6 +172,22 @@ fun Modifier.firstBaselineToTop(
   }
 )
 
+@Preview(showBackground = true)
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+  LayoutCodelabTheme {
+    Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextWithNormalPaddingPreview() {
+  LayoutCodelabTheme {
+    Text("Hi there!", Modifier.padding(top = 32.dp))
+  }
+}
+
 @Composable
 fun MyOwnColumn(
   modifier: Modifier = Modifier,
@@ -167,19 +212,81 @@ fun MyOwnColumn(
   }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun TextWithPaddingToBaselinePreview() {
-  LayoutCodelabTheme {
-    Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+fun StaggeredGrid(
+  modifier: Modifier = Modifier,
+  rows: Int = 3,
+  content: @Composable () -> Unit
+) {
+  Layout(
+    modifier = modifier,
+    content = content
+  ) { measureables, constraints ->
+
+    val rowWidths = IntArray(rows) { 0 }
+    val rowHeights = IntArray(rows) { 0 }
+
+    val placeables = measureables.mapIndexed { index, measurable ->
+      val placeable = measurable.measure(constraints)
+
+      val row = index % rows
+      rowWidths[row] += placeable.width
+      rowHeights[row] = max(rowHeights[row], placeable.height)
+
+      placeable
+    }
+
+    val width = rowWidths.maxOrNull()
+      ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+    val height = rowHeights.sumOf { it }
+      .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+    val rowY = IntArray(rows) { 0 }
+    for (i in 1 until rows) {
+      rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+    }
+
+    layout(width, height) {
+      val rowX = IntArray(rows) { 0 }
+
+      placeables.forEachIndexed { index, placeable ->
+        val row = index % rows
+        placeable.placeRelative(
+          x = rowX[row],
+          y = rowY[row]
+        )
+        rowX[row] += placeable.width
+      }
+    }
   }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun TextWithNormalPaddingPreview() {
+fun Chip(modifier: Modifier = Modifier, text: String) {
+  Card(
+    modifier = modifier,
+    border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+    shape = RoundedCornerShape(8.dp)
+  ) {
+    Row(
+      modifier = Modifier.padding(horizontal = 4.dp, vertical = 8.dp),
+      verticalAlignment = Alignment.CenterVertically
+    ) {
+      Box(
+        modifier = Modifier.size(16.dp, 16.dp)
+          .background(color = MaterialTheme.colors.secondary)
+      )
+      Spacer(Modifier.width(4.dp))
+      Text(text = text)
+    }
+  }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
   LayoutCodelabTheme {
-    Text("Hi there!", Modifier.padding(top = 32.dp))
+    Chip(text = "Hi there")
   }
 }
 
@@ -188,17 +295,5 @@ fun TextWithNormalPaddingPreview() {
 fun LayoutsCodelabPreview() {
   LayoutCodelabTheme {
     LayoutsCodelab()
-  }
-}
-
-@Preview(
-  showBackground = true,
-  widthDp = 320,
-  heightDp = 480
-)
-@Composable
-fun LazyListPreview() {
-  LayoutCodelabTheme {
-    LazyList()
   }
 }
