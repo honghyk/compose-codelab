e905568b60012a6d5c5d53604be8c2f3d2c5939e
honghyk <bhh970207@gmail.com>
2022-01-19 22:30:13 +0900

    step 7 - Custom Layout
    Compose UI does not permit multi-pass measurement.
    ### Layout Modifier
    - measurable: child to be measured and placed
    - constraints: minimum and maximum for the width and height of the child
    - placeable: The result of a `measure()` call on a `Measurable` that can be positioned

--- LayoutCodelab/app/src/main/java/com/example/layoutcodelab/MainActivity.kt
+++ LayoutCodelab/app/src/main/java/com/example/layoutcodelab/MainActivity.kt
@@ -25,7 +25,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.layoutcodelab.ui.theme.LayoutCodelabTheme
@@ -67,7 +72,8 @@ fun LayoutsCodelab() {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-  Column(modifier = modifier) {
+  MyOwnColumn(modifier = modifier.padding(8.dp)) {
+    Text("MyOwnColumn")
     Text(text = "Hi there!")
     Text(text = "Thanks for going through the Layouts codelab")
   }
@@ -117,6 +123,63 @@ fun ImageListItem(index: Int) {
     )
     Spacer(Modifier.width(10.dp))
     Text("Item $index", style = MaterialTheme.typography.subtitle1)
+  }
+}
+
+fun Modifier.firstBaselineToTop(
+  firstBaselineToTop: Dp
+) = this.then(
+  layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+      placeable.placeRelative(0, placeableY)
+    }
+  }
+)
+
+@Composable
+fun MyOwnColumn(
+  modifier: Modifier = Modifier,
+  content: @Composable () -> Unit
+) {
+  Layout(
+    modifier = modifier,
+    content = content,
+  ) { measurables, constraints ->
+    val placeables = measurables.map { measurable ->
+      measurable.measure(constraints)
+    }
+    var yPosition = 0
+
+    layout(constraints.maxWidth, constraints.maxHeight) {
+      placeables.forEach { placeable ->
+        placeable.placeRelative(x = 0, y = yPosition)
+
+        yPosition += placeable.height
+      }
+    }
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+  LayoutCodelabTheme {
+    Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextWithNormalPaddingPreview() {
+  LayoutCodelabTheme {
+    Text("Hi there!", Modifier.padding(top = 32.dp))
   }
 }
 
